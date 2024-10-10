5067cdd2a9b00e685aeeba9b42a14f3deb7decff
best9255 <phattarakorn.law@gmail.com>
2024-10-10 19:57:17 +0700

    Add project files.

---
+++ HashtableApp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HashtableApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hashtable hashtable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            hashtable.Add(keyData.Text,valueData.Text);
+            MessageBox.Show("complete");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            hashtable.Remove(keyData.Text);
+            MessageBox.Show("complete");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ICollection icollection = hashtable.Keys;
+            foreach (string key in icollection)
+            {
+                MessageBox.Show(hashtable[key].ToString());
+            }
+        }
+    }
+}
